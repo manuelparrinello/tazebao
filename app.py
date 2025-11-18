1985e227b46678ada83ebceea25f9aa22a6487f2
manuelparrinello <44732283+manuelparrinello@users.noreply.github.com>
2025-11-18 13:19:06 +0100

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request, flash
 from flask_cors import CORS
 import os
 from flask_sqlalchemy import SQLAlchemy
@@ -17,7 +17,6 @@ class Cliente(db.Model):
     __tablename__ = 'clienti'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    ragione_sociale = db.Column(db.String(150), nullable=True)
     email = db.Column(db.String(100), nullable=False)
     telefono = db.Column(db.String(20), nullable=False)
     lavori = db.relationship('Lavoro', backref='cliente', lazy=True)
@@ -31,9 +30,14 @@ class Lavoro(db.Model):
     __tablename__ = 'lavori'
     id = db.Column(db.Integer, primary_key=True)
     descrizione = db.Column(db.String(200), nullable=False)
-    data_inizio = db.Column(db.Date, nullable=False)
+    data_inizio = db.Column(db.Date, nullable=True)
     data_fine = db.Column(db.Date, nullable=True)
+    data_pagamento = db.Column(db.Date, nullable=True)
+    stato = db.Column(db.String(50), nullable=True)
+    priorita = db.Column(db.String(50), nullable=True)
+    preventivato = db.Column(db.Float, nullable=True)
     cliente_id = db.Column(db.Integer, db.ForeignKey('clienti.id'), nullable=False)
+    note = db.Column(db.Text, nullable=True)
     
     def __repr__(self):
         return f"<Lavoro {self.descrizione}>"
@@ -49,15 +53,74 @@ def index():
     return(render_template('index.html', title='Home', description='Welcome to the Home Page', path=db_path))
 
 # Nuovo cliente route
-@app.route('/cliente/new')
+@app.route('/cliente/new', methods=['GET', 'POST'])
 def nuovo_cliente():
-    return render_template('cliente_new.html')
+    if request.method == 'POST':
+        nomeCliente = request.form.get('nomeCliente').title()
+        telefono = request.form.get('telefono')
+        email = request.form.get('email').lower()
+        note = request.form.get('note')
+        colore = request.form.get('colore')
+        # Aggiungi il nuovo cliente al database
+        nuovo_cliente = Cliente(
+            name=nomeCliente,
+            telefono=telefono,
+            email=email,
+            note=note,
+            colore=colore
+        )
+        db.session.add(nuovo_cliente)
+        db.session.commit()
+        return jsonify({'message': 'Cliente aggiunto con successo!'}), 201
+    
+    if request.method == 'GET':
+        return render_template('cliente_new.html')
 
 # Clienti route
 @app.route('/clienti')
 def clienti():
     clienti_list = Cliente.query.all()
     return render_template('clienti.html', clienti=clienti_list)
+
+# Nuovo lavoro route
+@app.route('/lavoro/new', methods=['GET', 'POST'])
+def nuovo_lavoro():
+    if request.method == 'POST':
+        descrizione = request.form.get('descrizione')
+        data_inizio = request.form.get('data_inizio')
+        data_fine = request.form.get('data_fine')
+        data_pagamento = request.form.get('data_pagamento')
+        cliente_id = request.form.get('cliente_id')
+        priorita = request.form.get('priorita')
+        stato = request.form.get('stato')
+        preventivato = request.form.get('preventivato')
+        note = request.form.get('note')
+        
+        # Aggiungi il nuovo lavoro al database
+        nuovo_lavoro = Lavoro(
+            descrizione=descrizione,
+            data_inizio=data_inizio,
+            data_fine=data_fine,
+            data_pagamento=data_pagamento,
+            cliente_id=cliente_id,
+            priorita=priorita,
+            stato=stato,
+            preventivato=preventivato,
+            note=note
+        )
+        db.session.add(nuovo_lavoro)
+        db.session.commit()
+        return jsonify({'message': 'Lavoro aggiunto con successo!'}), 201
+    
+    if request.method == 'GET':
+        clienti_list = Cliente.query.all()
+        return render_template('lavoro_new.html', clienti=clienti_list)
+    
+# Lavori route
+@app.route('/lavori')
+def lavori():
+    lavori_list = Lavoro.query.all()
+    return render_template('lavori.html', lavori=lavori_list)
 
 # testing route
 @app.route('/test')
--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,35 @@
-const app = Vue.createApp({
+/*-------------------*/
+/* NUOVO CLIENTE APP */
+/*-------------------*/
+const nuovoClienteApp = Vue.createApp({
   data() {
     return {
       nomeCliente: "",
-      ragioneSociale: "",
       telefono: "",
       email: "",
-      color: "",
+      colore: "",
       note: "",
     };
   },
+  computed: {
+    inizialiNome() {
+      if (this.nomeCliente.length) {
+        return this.nomeCliente.charAt(0).toUpperCase();
+      } else {
+        return "-";
+      }
+    },
+  },
   methods: {
-    submitForm() {
+    submitForm(e) {
+      e.preventDefault();
       // Creazione di un oggetto FormData per inviare i dati del modulo
-      const formData = new FormData("#formCliente");
+      const form = document.querySelector("#nuovoCliente");
+      const formData = new FormData(form);
       formData.append("nomeCliente", this.nomeCliente);
-      formData.append("ragioneSociale", this.ragioneSociale);
       formData.append("telefono", this.telefono);
       formData.append("email", this.email);
-      formData.append("color", this.color);
+      formData.append("colore", this.colore);
       formData.append("note", this.note);
 
       // Invio della richiesta POST al server
@@ -27,6 +39,7 @@ const app = Vue.createApp({
       }).then((response) => {
         if (response.ok) {
           // Reindirizzamento alla pagina dei clienti dopo il successo
+          window.alert("Cliente creato con successo!");
           window.location.href = "/clienti";
         } else {
           console.error("Errore durante la creazione del cliente.");
@@ -35,4 +48,48 @@ const app = Vue.createApp({
     },
   },
   delimiters: ["[[", "]]"],
-}).mount("#cliente");
+}).mount("#formCliente");
+
+/*--------------------*/
+/*  NUOVO LAVORO APP  */
+/*--------------------*/
+const nuovoLavoroApp = Vue.createApp({
+  data() {
+    return {
+      descrizione: "",
+      dataInizio: "",
+      dataFine: "",
+      dataPagamento: "",
+      stato: "",
+      priorita: "",
+      preventivato: "",
+      cliente: "",
+    };
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      const form = document.querySelector("#nuovoLavoro");
+      const formData = new FormData(form);
+      formData.append("descrizione", this.descrizione);
+      formData.append("dataInizio", this.dataInizio);
+      formData.append("dataFine", this.dataFine);
+      formData.append("dataPagamento", this.dataPagamento);
+      formData.append("stato", this.stato);
+      formData.append("priorita", this.priorita);
+      formData.append("preventivato", this.preventivato);
+      formData.append("cliente", this.cliente);
+      fetch("/lavoro/new", {
+        method: "POST",
+        body: formData,
+      }).then((response) => {
+        if (response.ok) {
+          window.alert("Lavoro creato con successo!");
+          window.location.href = "/lavori";
+        } else {
+          console.error("Errore durante la creazione del lavoro.");
+        }
+      });
+    },
+  },
+}).mount("#formLavoro");

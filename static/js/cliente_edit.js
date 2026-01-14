867380c3c35b6f114f8801b9cdcee31b6afd8e62
manuelparrinello <44732283+manuelparrinello@users.noreply.github.com>
2026-01-14 12:19:22 +0100

    Writing cliente_edit.js function

--- static/js/cliente_edit.js
+++ static/js/cliente_edit.js
@@ -3,7 +3,7 @@ const getSingleCliente = Vue.createApp({
     return {
       cliente: {},
       error: null,
-      loading: true
+      loading: true,
     };
   },
   methods: {
@@ -11,7 +11,6 @@ const getSingleCliente = Vue.createApp({
       const root = document.querySelector("#clienteEditPage");
       const cliente_id = root.dataset.clienteId;
       const url = `/api/clienti/get/${cliente_id}`;
-      console.log(url);
       try {
         const response = await fetch(url, {
           method: "get",
@@ -23,11 +22,44 @@ const getSingleCliente = Vue.createApp({
           throw new Error(`Errore richiesta! (HTTP ${response.status})`);
         }
         this.cliente = await response.json();
-        console.log(this.cliente.count_lavori)
+        console.log(this.cliente.count_lavori);
       } catch (error) {
         this.error = error.message || "Errore imprevisto";
       } finally {
-        this.loading = false
+        this.loading = false;
+      }
+    },
+    editCliente(e, cliente_id) {
+      e.preventDefault();
+      const form = document.querySelector('#editClienteForm');
+      const formData = new FormData(form); 
+      formData.append("nomeCliente", this.cliente.nome); 
+      formData.append("telefono", this.cliente.telefono); 
+      formData.append("email", this.cliente.email); 
+      formData.append("note", this.cliente.note); 
+      formData.append("colore", this.cliente.colore);
+      const url = `/clienti/edit/${cliente_id}`;
+
+      const formDataJSON = {}
+      formData.forEach(function(value, key) {
+        formDataJSON[key] = value;
+      })
+      var json = JSON.stringify(formDataJSON);
+
+      console.log(json)
+      try {
+
+        // const response = await fetch(url, {
+        //   method: 'put',
+        //   body: formData,
+        // });
+
+
+
+      } catch {
+
+      } finally {
+
       }
     },
   },
@@ -35,5 +67,5 @@ const getSingleCliente = Vue.createApp({
     this.loadClienteData();
     console.log(this.loadClienteData());
   },
-  delimiters: ["[[", "]]"]
-}).mount('#clienteEditPage');
+  delimiters: ["[[", "]]"],
+}).mount("#clienteEditPage");

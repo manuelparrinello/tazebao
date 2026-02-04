c138e5a94b8cb2931ab0793402a8671284bb19e5
manuelparrinello <44732283+manuelparrinello@users.noreply.github.com>
2026-02-04 17:08:54 +0100

    sdqweqw

--- static/js/preventivo.js
+++ static/js/preventivo.js
@@ -1,104 +1,108 @@
 const preventivo = Vue.createApp({
-    data() {
-        return {
-            qty: "",
-            descrizione: "",
-            prezzo: "",
-            clienteData: {},
-            indirizzo_cliente: "",
-            p_iva_cliente: "",
-            sdi_cliente: "",
-            clienti: [],
-            loading: true,
-            righeLavoro: [],
-            idRiga: 0,
-            indiceRigheLavoro: 0,
-        };
+  data() {
+    return {
+      qty: "",
+      descrizione: "",
+      prezzo: "",
+      clienteData: {},
+      indirizzo_cliente: "",
+      p_iva_cliente: "",
+      sdi_cliente: "",
+      clienti: [],
+      idRiga: 0,
+      righeLavoro: [],
+      indiceRigheLavoro: 0,
+      loading: true,
+    };
+  },
+  methods: {
+    async sendForm(e) {
+      e.preventDefault();
+      const url = `/preventivi/nuovo`;
+      const form = document.querySelector("#formNuovoPreventivo");
+      const formData = new FormData(form);
+      const response = await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+      if (!response.ok) {
+        throw new Error("Errore richiesta: HTTP " + response.status);
+      }
+      const data = await response.json();
+      let target_url = data["target_url"];
+      window.open(target_url, `_blank`);
     },
-    methods: {
-        async sendForm(e) {
-            e.preventDefault();
-            const url = `/preventivi/nuovo`;
-            const form = document.querySelector("#formNuovoPreventivo");
-            const formData = new FormData(form);
-            const response = await fetch(url, {
-                method: 'POST',
-                body: formData,
-            });
-            if (!response.ok) {
-                throw new Error("Errore richiesta: HTTP " + response.status);
-            }
-            const data = await response.json()
-            let target_url = data['target_url'];
-            window.open(target_url, `_blank`)
+
+    async loadClienti() {
+      const url = `/api/clienti/getall`;
+      const response = await fetch(url, {
+        method: "get",
+        headers: {
+          Accept: "application/json",
         },
+      });
+      if (!response.ok) {
+        throw new Error("Errore HTTP:" + response.status);
+      }
+      const data = await response.json();
+      this.clienti = data;
+      for (const cliente of this.clienti) {
+      }
+    },
 
-        async loadClienti() {
-            const url = `/api/clienti/getall`;
-            const response = await fetch(url, {
-                method: 'get',
-                headers: {
-                    'Accept': 'application/json'
-                }
-            })
-            if (!response.ok) {
-                throw new Error('Errore HTTP:' + response.status);
-            }
-            const data = await response.json();
-            this.clienti = data;
-            for (const cliente of this.clienti) {}
-        },
+    async loadClienteDataByID(e) {
+      const id = e.target.value;
+      const url = `/api/clienti/get/${id}`;
 
-
-        async loadClienteDataByID(e) {
-            const id = e.target.value;
-            const url = `/api/clienti/get/${id}`;
-
-            try {
-                const response = await fetch(url, {
-                    method: 'get',
-                    content: 'application/json'
-                });
-                if (!response.ok) {
-                    throw new Error("Errore HTML:" + response.status);
-                };
-                const data = await response.json();
-                this.clienteData = data;
-            } catch (error) {
-                console.log(error.message);
-            } finally {
-                this.loading = false
-            }
-        },
-
-        calcSubtotale() {
-            var parziali = [];
-            for (const element of this.righeLavoro) {
-                parziali.push(element.totaleRiga);
-            }
-            return parziali
-        },
-
-        async addRigaPreventivo(e) {
-            e.preventDefault();
-            const form = document.querySelector('#formNuovoPreventivo');
-            const formData = new FormData(form);
-            const indiceRigheLavoro = 0;
-            const riga = {
-                idRiga: this.idRiga,
-                qty: formData.get('qty'),
-                descrizione: formData.get('descrizione'),
-                prezzo: formData.get('prezzo'),
-                totaleRiga: Number(formData.get('qty')) * Number(formData.get('prezzo')),
-            };
-            this.righeLavoro.push(riga);
-            console.log(this.righeLavoro[indiceRigheLavoro].totaleRiga);
-            this.indiceRigheLavoro++;
-            this.idRiga++;
+      try {
+        const response = await fetch(url, {
+          method: "get",
+          content: "application/json",
+        });
+        if (!response.ok) {
+          throw new Error("Errore HTML:" + response.status);
         }
+        const data = await response.json();
+        this.clienteData = data;
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        this.loading = false;
+      }
     },
-    mounted() {
-        this.loadClienti();
+
+    calcSubtotale() {
+      const parziali = [];
+      for (const element of this.righeLavoro) {
+        parziali.push(element.totaleRiga);
+        return parziali;
+      }
     },
-    delimiters: ["[[", "]]"],
+
+    calcIdRiga() {
+      for (const el of this.righeLavoro) {
+        return el.idRiga;
+      } el.idRiga ++
+    },
+
+    async addRigaPreventivo(e) {
+      e.preventDefault();
+      const form = document.querySelector("#formNuovoPreventivo");
+      const formData = new FormData(form);
+      const riga = {
+        idRiga: this.idRiga,
+        qty: formData.get("qty"),
+        descrizione: formData.get("descrizione"),
+        prezzo: formData.get("prezzo"),
+        totaleRiga: Number(formData.get("qty")) * Number(formData.get("prezzo")),
+      };
+      this.righeLavoro.push(riga);
+      this.indiceRigheLavoro++;
+      console.log(this.calcSubtotale());
+    },
+  },
+  mounted() {
+    this.loadClienti();
+  },
+  delimiters: ["[[", "]]"],
 }).mount("#nuovoPreventivo");
